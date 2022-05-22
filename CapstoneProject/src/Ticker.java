dc219ab4327dd4c7b17d14332b1980a4def21d41
Ryan Xu <89160581+rxu770@users.noreply.github.com>
2022-05-21 17:41:57 -0700

    javadoc comments for Ticker.java

--- CapstoneProject/src/Ticker.java
+++ CapstoneProject/src/Ticker.java
@@ -11,17 +11,30 @@ public class Ticker {
 	
 	public static String ticker;
 	
+	/**
+	 * Creates a blank ticker
+	 */
 	public Ticker() {
 		ticker = "";
 	}
 	
+	/**
+	 * Returns the ticker
+	 * @return ticker of the stock
+	 */
+	public String getTicker() {
+		return ticker;
+	}
+	
+	/**
+	 * Sets the ticker to a given string
+	 * @param s string to set ticker to
+	 */
 	public void setTicker(String s) {
 		System.out.println("set");
 		ticker = s;
 	}
-	public String getTicker() {
-		return ticker;
-	}
+	
 	
 
 	
