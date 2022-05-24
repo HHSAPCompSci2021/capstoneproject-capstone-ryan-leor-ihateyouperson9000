c48526a9eeaf8aee903262760a3bbac4e59f2483
Ryan Xu <89160581+rxu770@users.noreply.github.com>
2022-05-23 22:17:55 -0700

    fixed axes

--- CapstoneProject/src/DrawingSurface.java
+++ CapstoneProject/src/DrawingSurface.java
@@ -318,10 +318,10 @@ public class DrawingSurface extends PApplet {
 	public void drawAxes() {
 		int yDiff = chart.getMaxYDrawn()-chart.getMinYDrawn();
 		double increment = yDiff/5.0;
-		for (int i = 0; i < 5; i++) {
-			Line l = new Line(150, chart.getMaxYDrawn()-(i*increment), 750, chart.getMaxYDrawn()-(i*increment));
+		for (int i = 0; i < 6; i++) {
+			Line l = new Line(150, chart.getMinYDrawn()+(i*increment), 750, chart.getMinYDrawn()+(i*increment));
 			// System.out.println(chart.getMaxYDrawn());
-			System.out.println(chart.getMaxYDrawn());
+//			System.out.println(chart.getMaxYDrawn());
 			// System.out.println(chart.getMaxYDrawn()-(i*increment) + ": " + i);
 			l.setStrokeColors(255, 255, 255);
 			l.draw(this);
